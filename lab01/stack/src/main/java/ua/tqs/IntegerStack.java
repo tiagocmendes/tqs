2e3f47742e110ae805791778a1ecf3fe342d61f7
tiagocmendes <tiagocmendes@ua.pt>
2020-02-15 13:45:02 +0000

    Finished first exercise

--- lab01/stack/src/main/java/ua/tqs/IntegerStack.java
+++ lab01/stack/src/main/java/ua/tqs/IntegerStack.java
@@ -1,6 +1,9 @@
 package ua.tqs;
 
+import org.omg.CORBA.INTERNAL;
+
 import java.lang.reflect.Array;
+import java.util.NoSuchElementException;
 
 public class IntegerStack implements StackInterface<Integer> {
 
@@ -14,16 +17,24 @@ public class IntegerStack implements StackInterface<Integer> {
     }
 
     public void push(Integer x) {
+        if(this.isFull())
+            throw new IllegalStateException();
         this.stack[++this.topElement] = x;
     }
 
     public Integer pop() {
+
+        if(this.isEmpty())
+            throw new NoSuchElementException();
+
         Integer n = this.stack[this.topElement];
         this.stack[this.topElement--] = 0;
         return n;
     }
 
     public Integer peek() {
+        if(this.isEmpty())
+            throw new NoSuchElementException();
         return this.stack[this.topElement];
     }
 
@@ -33,6 +44,10 @@ public class IntegerStack implements StackInterface<Integer> {
 
     public boolean isEmpty() {
         return this.topElement == -1;
+    }
+
+    public boolean isFull() {
+        return this.topElement == this.maxSize - 1;
     }
 
 }
--- lab01/stack/src/test/java/ua/tqs/IntegerStackTest.java
+++ lab01/stack/src/test/java/ua/tqs/IntegerStackTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,27 +28,107 @@ class IntegerStackTest {
     @Test
     void push() {
         System.out.println("----------PUSH TEST----------");
-        IntegerStack stack = new IntegerStack(1);
-        stack.push(30);
-        System.out.println("Top element: " + stack.peek());
-    }
-
-
-    @Test
-    void pop() {
-    }
-
-    /*
-    @Test
-    void peek() {
+        int element = 30;
+        this.stack.push(element);
+        assertEquals((Integer) element, stack.peek());
     }
 
     @Test
-    void size() {
+    void stackEmptyOnConstruction() {
+        System.out.println("----------STACK EMPTY ON CONSTRUCTION TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        assertEquals(true, localStack.isEmpty());
     }
 
     @Test
-    void isEmpty() {
+    void sizeZeroOnConstruction() {
+        System.out.println("----------SIZE ZERO ON CONSTRUCTION TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        assertEquals(0, localStack.size());
     }
-    */
+
+    @Test
+    void severalPushes() {
+        System.out.println("----------SEVERAL PUSHES TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        int n = 10; // 10 pushes
+        for(int i = 0; i < n; i++)
+            localStack.push(i);
+        assertEquals(false, localStack.isEmpty());
+        assertEquals(n, localStack.size());
+    }
+
+    @Test
+    void pushThenPop() {
+        System.out.println("----------PUSH THEN POP TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        int x = 243;    // value to push
+        localStack.push(x);
+        assertEquals((Integer)x, localStack.pop());
+    }
+
+    @Test
+    void pushThenPeek() {
+        System.out.println("----------PUSH THEN PEEK TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        int x = 243;    // value to push
+        localStack.push(x);
+        int oldSize = localStack.size();
+        assertEquals((Integer)x, localStack.peek());
+        assertEquals(oldSize, localStack.size());
+    }
+
+    @Test
+    void severalPops() {
+        System.out.println("----------SEVERAL POPS TEST----------");
+        IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        int n = 10; // 10 pushes
+
+        // several pushes
+        for(int i = 0; i < n; i++)
+            localStack.push(i);
+        assertEquals(false, localStack.isEmpty());
+        assertEquals(n, localStack.size());
+
+        // several pops
+        for(int i = 0; i < n; i++)
+            localStack.pop();
+        assertEquals(true, localStack.isEmpty());
+        assertEquals(0, localStack.size());
+    }
+
+    @Test
+    void popFromEmptyStack() {
+        System.out.println("----------POP FROM EMPTY STACK TEST----------");
+        final IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        assertThrows(NoSuchElementException.class, new Executable() {
+            public void execute() throws Throwable {
+                localStack.pop();
+            }
+        });
+    }
+
+    @Test
+    void peekFromEmptyStack() {
+        System.out.println("----------PEEK FROM EMPTY STACK TEST----------");
+        final IntegerStack localStack = new IntegerStack(this.MAX_SIZE);
+        assertThrows(NoSuchElementException.class, new Executable() {
+            public void execute() throws Throwable {
+                localStack.peek();
+            }
+        });
+    }
+
+    @Test
+    void pushToFullStack() {
+        System.out.println("----------PUSH TO FULL STACK TEST----------");
+        final IntegerStack localStack = new IntegerStack(1);
+        localStack.push(123);
+        assertThrows(IllegalStateException.class, new Executable() {
+            public void execute() throws Throwable {
+                localStack.push(69);
+            }
+        });
+    }
+
 }
